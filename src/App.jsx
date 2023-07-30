ba94c2fb8a757c0273a80e4245139babbaa7be95
Vasanth Kumar <lakavath20386@iiitd.ac.in>
2023-07-30 18:51:13 +0530

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path="/" element = {} ></Route>
+     </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
